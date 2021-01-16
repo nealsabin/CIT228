2c2713d2ec9da5727032a423aa7bf57503c35fc9
Neal Sabin <sabin7@mail.nmc.edu>
2021-01-15 19:49:27 -0500

    initial commit

---
+++ Lesson1/numberFun.py
@@ -0,0 +1,46 @@
+import random
+firstNumber = random.randrange(1,100)
+secondNumber = random.randrange(1,100)
+print("\nFirst number =",firstNumber)
+print("Second number =",secondNumber)
+
+division = firstNumber / secondNumber
+print(firstNumber,"/", secondNumber,  "=" , division)
+
+floor = firstNumber // secondNumber
+print(firstNumber,"//", secondNumber,  "=" , floor)
+
+modulus = firstNumber % secondNumber
+print(firstNumber,"%", secondNumber,  "=" , modulus)
+
+addition = firstNumber + secondNumber
+print(firstNumber,"+", secondNumber,  "=" , addition)
+
+subtraction = firstNumber - secondNumber
+print(firstNumber,"-", secondNumber,  "=" , subtraction)
+
+multiplication = firstNumber * secondNumber
+print(firstNumber,"*", secondNumber,  "=" , multiplication)
+
+NUMBER3 = 25
+secondNumber = NUMBER3
+print("\nFirst number =",firstNumber)
+print("Second number =",secondNumber)
+
+division = firstNumber / secondNumber
+print(firstNumber,"/", secondNumber,  "=" , division)
+
+floor = firstNumber // secondNumber
+print(firstNumber,"//", secondNumber,  "=" , floor)
+
+modulus = firstNumber % secondNumber
+print(firstNumber,"%", secondNumber,  "=" , modulus)
+
+addition = firstNumber + secondNumber
+print(firstNumber,"+", secondNumber,  "=" , addition)
+
+subtraction = firstNumber - secondNumber
+print(firstNumber,"-", secondNumber,  "=" , subtraction)
+
+multiplication = firstNumber * secondNumber
+print(firstNumber,"*", secondNumber,  "=" , multiplication)
---
+++ Lesson1/stringFun.py
@@ -0,0 +1,38 @@
+#exercise 1
+print("-------------------------------------")
+print("Exercise 1")
+name="neal sabin"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("My first initial is: ", name[0].upper())
+
+#exercise 2
+print("-------------------------------------")
+print("Exercise 2")
+noun = "fox "
+verb = "jumped "
+adj = "quick"
+ending = "over the wall."
+sentence1 = "the " + adj + " brown " + noun + verb + ending
+print(sentence1.upper())
+print(sentence1.lower())
+
+#exercise 3
+print("-------------------------------------")
+print("Exercise 3")
+first = "The mystery of life isn't "
+second = "\n\ta problem to solve, "
+third = "\n\t\tbut a reality to experience."
+sentence3 = first + second + third
+print(sentence3.title())
+print(sentence3.upper())
+
+#exercise 4
+print("-------------------------------------")
+print("Day\t\tWeather")
+print("---\t\t-------")
+print("Sunday\t\tSunny")
+print("Monday\t\tSnow")
+print("Tuesday\t\tRain")
+print("-------------------------------------")
